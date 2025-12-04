83d9513fbfe8f3ea950fffea4a1a3bbc270deb69
kaihere14 <yashpalsharma9950@gmail.com>
2025-12-04 17:02:46 +0530

    fix: update default server port from 3000 to 5000

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import statusMonitor from "express-status-monitor";
 
 const app = express();
 app.use(statusMonitor());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
