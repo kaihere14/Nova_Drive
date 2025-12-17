eb91cd09814dfd8a4a35e2003cb858b75789d873
Arman Thakur <yashpalsharma9950@gmail.com>
2025-12-17 08:23:40 +0530

    chore(index): correct typo in CI/CD setup comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -42,7 +42,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-//testing commit
+//testiing ci/cd setup
 
 app.use("/api/chunks", chunkRoutes);
 app.use("/api/user", userRouted);
